29473cc2e132187bc768b3de8f028ef72e0ced45
RaulRufoEnciso <rrufo19@jaumebalmes.net>
2023-01-31 19:07:39 +0100

    carpetas creadas, exports y imorts creados, todo funciona

---
+++ activitat_pastisers/src/Components/Imagen.jsx
@@ -0,0 +1,7 @@
+/* Componente que devuelve una imagen */
+export const Imagen = () => {
+    return(
+    <div>
+      <img src="Imagenes/espagetis.jpg" width={150} height={150} alt={"Espaguetis al pesto con una rica cerveza de fondo"} ></img>
+    </div>)
+  }
